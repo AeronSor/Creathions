3630c114f14d50c272a3b5c520aca954ea353c78
Aeron Sor <aeronsor@gmail.com>
2023-05-22 20:43:41 -0300

    Changed the structure once again because I discovered I can separate client from server from packwiz itself

--- server/fetch-mods.sh
+++
@@ -1,5 +0,0 @@
-while read name; do
-	packwiz -y cf install "$name"
-done <modlist.txt
-packwiz cf export
-mv *.zip ../
--- update.sh
+++ update.sh
@@ -1,15 +1,5 @@
-# Client
-cd ./client
-while read name; do
-	packwiz -y cf install "$name"
-done <../modlist-client.txt
-packwiz cf export
-mv *.zip ../
-
 #Server
-cd ../server
 while read name; do
 	packwiz -y cf install "$name"
-done <../modlist-server.txt
+done <modlist.txt
 packwiz cf export
-mv *.zip ../
